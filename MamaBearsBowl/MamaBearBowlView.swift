c40a024553a8bc22b1c36e723b17ccfb07e38604
Nathan Smith <nsmith@lcs.on.ca>
2021-12-11 16:00:38 -0500

    NOT CURRENTLY WORKING, NEED TO ASK MR GORDON OR ANGUS ABOUT IT

--- MamaBearsBowl/MamaBearBowlView.swift
+++ MamaBearsBowl/MamaBearBowlView.swift
@@ -19,17 +19,52 @@ struct ContentView: View {
     @State var bowlThreeWeight: Double = 61.00
     
     @State var result: String = ""
+    
+    @State var isResultShowing = false
+    
     // MARK: COMPUTED PROPERTIES
+    let b1 = Int(bowlOneWeight())
+    let b2 = Int(bowlTwoWeight())
+    let b3 = Int(bowlThreeWeight())
+    
+    var arr = [
+        
+        b1()
+        
+        ,
+        
+        b2()
+        
+        ,
+        
+        b3()
+    ]
+    let min = arr.min()
+    let mac = arr.max()
+    let median = arr.sorted(by: <)[arr.count / 2]
+    
+    var mamaBowl: String {
+        if median == bowlOneWeight {
+            result = "Bowl 1 is Mama Bear's bowl!"
+        } else if median == bowlTwoWeight {
+            result = "Bowl 2 is Mama Bear's bowl!"
+        } else if median ==  bowlThreeWeight {
+            result = "Bowl 3 is Mama Bear's bowl!"
+        } else {
+            result = "error"
+        }
+    }
+    
     var body: some View {
         ZStack {
             Color("Sand")
                 .ignoresSafeArea()
             VStack {
                 HStack(alignment: .top) {
-                Text("Mama Bear Bowl Finder")
-                    .foregroundColor(Color("Concord"))
-                    .padding(.top)
-                    .font(.system(size: 30, weight: .medium, design: .serif))
+                    Text("Mama Bear Bowl Finder")
+                        .foregroundColor(Color("Concord"))
+                        .padding(.top)
+                        .font(.system(size: 30, weight: .medium, design: .serif))
                     Image(systemName: "magnifyingglass")
                         .resizable()
                         .scaledToFill()
@@ -83,16 +118,24 @@ struct ContentView: View {
                 }
                 Divider()
                 Button(action: {
-                    
+                    isResultShowing = true
                 }, label: {
-                    Text("Find Mama Bear's bowl")
+                    Text("Find Mama Bear's bowld")
                 })
                     .background(ButtonBackground())
                     .buttonStyle(.bordered)
                     .foregroundColor(Color("Concord"))
                     .cornerRadius(11)
                     .padding(.top)
+                
+                HStack (spacing: 40){
+                    Text("\(String(format: "%.1f", median))")
+                    Text(result)
+                        .font(.system(size: 20, weight: .medium, design: .serif))
+                        .foregroundColor(Color("Concord"))
+                }
                 Spacer(minLength: 480)
+                
             }
         }
     }
