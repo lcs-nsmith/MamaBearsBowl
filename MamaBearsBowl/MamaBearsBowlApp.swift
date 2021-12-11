dcf6bdf4f250cc6498ea0ef0bbae3106d896b2a1
Nathan Smith <nsmith@lcs.on.ca>
2021-12-11 09:52:43 -0500

    Initial Commit

---
+++ MamaBearsBowl/MamaBearsBowlApp.swift
@@ -0,0 +1,17 @@
+//
+//  MamaBearsBowlApp.swift
+//  MamaBearsBowl
+//
+//  Created by Nate S on 2021-12-11.
+//
+
+import SwiftUI
+
+@main
+struct MamaBearsBowlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
